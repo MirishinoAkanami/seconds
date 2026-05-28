e7132e544f9e999c0bd6c0ff5b61e17758c4e491
EJ Lawrence Comandante <ejlawrencecomandante054@gmail.com>
2026-05-28 16:46:59 +0800

    settings.py debug

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,20 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ── API Keys (set as environment variables on Render) ─────
